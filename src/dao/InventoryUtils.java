63ea16d256c9b3f01061870e4467740e1022257d
piyumeesathyanjalee <piyumeesathyanjalee1999@gmail.com>
2024-09-09 12:39:37 +0530

    Add files via upload

---
+++ src/dao/InventoryUtils.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author User
+ */
+public class InventoryUtils {
+    public static String billPath = "E:\\COT KANDY 2023\\Inventory Management System";
+}
